260e49719898fc4a0f29162a47e98e8f51626711
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 23:11:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,6 @@ def using_flatten(array)
   return array.flatten
 end
 
-def using_delete_at(array, string)
-  return array.delete(string)
+def using_delete_at(array, int)
+  return array.delete
 end
