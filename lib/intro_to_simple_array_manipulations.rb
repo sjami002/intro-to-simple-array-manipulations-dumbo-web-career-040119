39f4b62d45519ae618005add19b0af76b50a59fb
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 23:08:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,11 @@ end
 def using_concat(array1, array2)
   return array1.concat(array2)
 end
+
+def using_insert(array, element)
+  return array.insert(element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
